4f8ebc9bbfe675c86f6e3b6bdbd1efd1ff60de04
kungfukitty <46254915+kungfukitty@users.noreply.github.com>
2025-09-17 10:24:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  server: { host: true, port: 5173 }
 })
